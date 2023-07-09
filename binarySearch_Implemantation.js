875b465f3bd1b3281d01a8b8abdd21e2cc59fa8c
Umut ESEN <75943806+uesen94@users.noreply.github.com>
2023-07-09 16:09:22 +0300

    Create binarySearch_Implemantation.js

---
+++ binarySearch_Implemantation.js
@@ -0,0 +1,31 @@
+function binarySearch(arr,target){
+    let left=0;
+    let right=arr.length-1;
+    
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(arr[mid]===target){
+            return mid;
+        }
+        if(arr[mid]<target){
+            left=mid+1;
+        }else{
+            right=mid-1;
+        }
+}
+    return -1;
+}
+
+function nullCheck(result){
+    if (result === -1) {
+        console.log("The target is not found in the list.");
+    } else {
+        console.log("The target is found at index " + result + ".");
+    }
+}
+
+const arr = [1, 2, 3, 4, 5,6];
+const target = 6;
+const searchResult = binarySearch(arr, target);
+nullCheck(searchResult);
+
