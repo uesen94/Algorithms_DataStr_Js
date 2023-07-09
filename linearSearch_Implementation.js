67f6ce3d5bdc82d964c5bdd8d49c9c7154d2e7d2
Umut ESEN <75943806+uesen94@users.noreply.github.com>
2023-07-09 16:08:20 +0300

    Create linearSearch_Implementation.js

---
+++ linearSearch_Implementation.js
@@ -0,0 +1,22 @@
+function linearSearch(arr,key) {
+        for(let i=0;i<=arr.length;i++){
+            if(arr[i]===key){
+                return i;
+            }
+        }
+    return -1;
+}
+
+function nullCheck(result){
+    if (result === -1) {
+        console.log("The target is not found in the list.");
+    } else {
+        console.log("The target is found at index " + result + ".");
+    }
+}
+
+// Example usage
+const arr = [1, 2, 3, 4, 5];
+const key = 5;
+const searchResult = linearSearch(arr, key);
+nullCheck(searchResult);
