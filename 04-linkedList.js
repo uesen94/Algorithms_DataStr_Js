f644c00b7aa04e0b26f8e0e708902e98ec064d98
Umut ESEN <75943806+uesen94@users.noreply.github.com>
2023-07-09 17:53:02 +0300

    Update 04-linkedList.js
    adding Insert,Remove implementation and also adding Search algorithm

--- 04-linkedList.js
+++ 04-linkedList.js
@@ -10,19 +10,78 @@ class LinkedList {
     this.head = null; // Linked list'in başını temsil eder.
   }
 
-
- add(data) {
-    const newNode = new Node(data); // Yeni bir düğüm oluşturulur
-
-    if (this.head === null) {
-      this.head = newNode; // Eğer linked list boşsa, yeni düğümü baş olarak atarız
+//Adding Node to the linkedList
+   add(data) {
+      const newNode = new Node(data); // Yeni bir düğüm oluşturulur
+  
+      if (this.head === null) {
+        this.head = newNode; // Eğer linked list boşsa, yeni düğümü baş olarak atarız
+      } else {
+        let current = this.head;
+        while (current.next !== null) {
+          current = current.next; // linked list'in son düğümüne ilerleriz
+        }
+        current.next = newNode; // Son düğümün next bağlantısını yeni düğüme yönlendiririz
+      }
+    }
+  
+  //Inserting a Node
+  insert(data, position) {
+    const newNode = new Node(data);
+    if (position === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
     } else {
       let current = this.head;
-      while (current.next !== null) {
-        current = current.next; // linked list'in son düğümüne ilerleriz
+      let previous = null;
+      let index = 0;
+      while (index < position && current !== null) {
+        previous = current;
+        current = current.next;
+        index++;
       }
-      current.next = newNode; // Son düğümün next bağlantısını yeni düğüme yönlendiririz
+      if (current === null) {
+        previous.next = newNode;
+      } else {
+        newNode.next = current;
+        previous.next = newNode;
+      }
     }
+  }
+  
+  //Removing a Node
+  remove(positionRemove) {
+    if (positionRemove === 0) {
+      if (this.head !== null) {
+        this.head = this.head.next;
+      }
+    } else {
+      let current = this.head;
+      let previous = null;
+      let index = 0;
+      while (index < positionRemove && current !== null) {
+        previous = current;
+        current = current.next;
+        index++;
+      }
+      if (current !== null) {
+        previous.next = current.next;
+      }
+    }
+  }
+
+  //Implementing Search Algorithm
+  search(key){
+    let current=this.head;
+    let index=0;
+    while (current.next !== null) {
+     if (current.data === key) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+    return -1;
   }
 
   print() {
@@ -34,10 +93,31 @@ class LinkedList {
   }
 }
 
-// Kullanım örneği
-const linkedList = new LinkedList();
-linkedList.add(10);
-linkedList.add(20);
-linkedList.add(30);
-linkedList.print(); // 10 20 30 çıktısını verir
+  // Kullanım örneği
+  const linkedList = new LinkedList();
+  linkedList.add(10);
+  linkedList.add(20);
+  linkedList.add(30);
+  console.log("Before insertion:");
+  linkedList.print(); // 10 20 30 çıktısını verir
+  
+  const newData = 15;
+  const position = 1;
+  linkedList.insert(newData, position);
+  
+  console.log("After insertion:");
+  linkedList.print(); // 10 15 20 30
+  
+  const positionRemove=2;
+  linkedList.remove(positionRemove);
+  console.log("After removal:");
+  linkedList.print(); // 10 15 30
+  
+  const key = 20;
+  const result = linkedList.search(key);
+  if (result !== -1) {
+    console.log(`The key ${key} is found at index ${result}.`);
+  } else {
+    console.log(`The key ${key} is not found in the list.`);
+  }
 
