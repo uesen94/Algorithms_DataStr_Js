abeb5609801f2c1800ed11b315bb769fca70edbb
Umut ESEN <75943806+uesen94@users.noreply.github.com>
2023-07-11 02:06:33 +0300

    Create 09-quickSort.js

---
+++ 09-quickSort.js
@@ -0,0 +1,45 @@
+//depends on divide and conquer(böl ve yönet)
+//complexity:O(logn), worst case O(log n^2)
+//selection of pivot element is crucial for effeciency
+ //it will be roughly the radian of an array.
+
+ function quickSort(numbers) {
+    if(numbers.length <=1){
+        return numbers;
+    }
+
+     const pivotIndex= Math.floor(numbers.length/2);
+     const pivot= numbers[pivotIndex];
+     const left= [];
+     const right= [];
+     
+
+     for (let i=0; i<=numbers.length; i++){
+         if(i === pivotIndex){
+             continue;
+         }
+         
+         if(numbers[i] < pivot){
+             left.push(numbers[i]);
+         }
+           
+         else{
+             //console.log(numbers[i]);
+             right.push(numbers[i]); 
+         }
+             
+     }
+     
+     const sortedLeft=quickSort(left);
+     const sortedRight=quickSort(right);
+
+     return [ ...sortedLeft, pivot, ...sortedRight];
+ }
+
+
+
+
+ //testing
+ var testedValues=[3,5,1,2,4];
+ var sorted=quickSort(testedValues);
+ console.log(sorted);
